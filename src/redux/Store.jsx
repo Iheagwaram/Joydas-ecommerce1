ef4c2b887f6d68e8084e6bc84c703b5aa33e79c3
Iheagwaram <tiheagwaram5@gmail.com>
2025-02-14 16:04:10 +0100

    ninth commit

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from './slices/Cartslices.jsx'
+import cartSlice from "./slices/Cartslices";
 
-const store = configureStore(
-    {
-        reducer: {
-            cart: cartSlice
-        }
-    }
-)
+const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+  },
+});
 
 export default store;
